a336608ad122771f3bf7878ec35cf18b68f4f96c
Angel Dominguez <angeldominguezg@gmail.com>
2025-06-23 09:13:44 -0400

    Styled components props and css functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: var(--color-grey-0);
+  gap: 1rem;
+`;
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis!</Heading>
+        <Heading as="h2" >Check in/out</Heading>
+        <Button onClick={() => alert("Check In")}>Check In</Button>
+        <Button onClick={() => alert("Check Out")}>Check Out</Button>
+        <Heading as="h3">Check in/out</Heading>
+        <Input type="number" placeholder="Number Of Guests" />
+      </StyledApp>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,31 @@
+import styled, {css} from 'styled-components';
+
+// const test = css`
+//   text-decoration: underline;
+//   background-color: yellow;
+// `;
+
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+  ${(props) =>
+    props.as === "h2" &&
+    css`
+      font-size: 3rem;
+      font-weight: 500;
+    `}
+  ${(props) =>
+    props.as === "h3" &&
+    css`
+      font-size: 2rem;
+      font-weight: 400;
+    `}
+
+  line-height: 1.5;
+`;
+
+export default Heading;
