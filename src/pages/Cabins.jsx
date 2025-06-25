b60ec2fe9e519c551e8eff286454bd340867aa27
Angel Dominguez <angeldominguezg@gmail.com>
2025-06-25 09:29:19 -0400

    Fetching Cabin data React Query

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,27 +1,18 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(() => {
-    async function fetchCabins() {
-      try {
-        const data = await getCabins();
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchCabins();
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://mmbmwaytrjwsfgarltjz.supabase.co/storage/v1/object/public/cabins-images//cabin-001.jpg" alt="" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter and Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,7 +1,7 @@
 import supabase from "./supabase";
 
 export async function getCabins() {
-  const { data, error } = await supabase.from("cabins").select("*").single();
+  const { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
     console.error(error);
