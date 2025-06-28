081e9c42743f81ed9ca93ad76632681eb21d51ec
Angel Dominguez <angeldominguezg@gmail.com>
2025-06-28 18:26:13 -0400

    Uploading images for cabins to supabase

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -8,36 +8,57 @@ export async function getCabins() {
     throw new Error("Cabin could not be loaded");
   }
   return data;
-};
+}
 
 export async function deleteCabin(id) {
-  const { data, error } = await supabase
-    .from("cabins")
-    .delete()
-    .eq("id", id);
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be deleted");
   }
   return data;
-};
+}
 
 export async function createCabin(newCabin) {
+  // https://mmbmwaytrjwsfgarltjz.supabase.co/storage/v1/object/public/cabins-images//cabin-001.jpg
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabins-images//${imageName}`;
+
+  // 1- Create the cabin
   const { data, error } = await supabase
-    .from('cabins')
-    .insert([
-      newCabin,
-    ])
+    .from("cabins")
+    .insert([{...newCabin, image: imagePath}])
     .select();
 
-    if (error) {
-      console.error(error);
-      throw new Error("Cabin could not be created");
-    }
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
+  }
+
+  // 2- Is the creation ok then upload the cabin image.
+  const { data: storageData, error: storageError } = await supabase.storage
+    .from("cabins-images")
+    .upload(imageName, newCabin.image);
+
+    if (storageError) {
+    // Handle error
+    // Delete the cabin if there was an error uploading the corresponding image
+    await supabase.from("cabins").delete().eq("id", newCabin.id);
+    console.error(error);
+    throw new Error("Cabin could not be created because a file error");
+
+  } else {
+    // Handle success
     return data;
-};
 
-export async function updateCabin() {
+  }
 
-};
+  // return data;
+}
+
+export async function updateCabin() {}
