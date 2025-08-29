a7af8bfaae1f280062f130e611a5f7a32800c7ac
Angel Dominguez <angeldominguezg@gmail.com>
2025-08-29 18:41:52 -0400

    Implementing stadistics, computed booking and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +10,34 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+
+  const {bookings, isLoading} = useRecentBookings();
+  const {stays, confirmedStays, isLoadingStays} = useRecentStays();
+
+  if(isLoading || isLoadingStays) return <Spinner />
+
+  // console.log("Bookings", bookings);
+  // console.log("Stays", stays);
+  // console.log("ConfirmedStays", confirmedStays);
+
+  return (
+    <StyledDashboardLayout>
+      <div>
+        Statistics
+      </div>
+      <div>
+        Today's Activity
+      </div>
+      <div>
+        Chart Stays duration
+      </div>
+      <div>
+        Chart Sales
+      </div>
+    </StyledDashboardLayout>
+  )
+}
+
+export default DashboardLayout
