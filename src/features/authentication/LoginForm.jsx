d9c278fcc6335c7430101148ba384e3240ca2f2a
Angel Dominguez <angeldominguezg@gmail.com>
2025-09-03 08:42:24 -0400

    Removed development credentials

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("sisohi8254@cavoyar.com");
-  const [password, setPassword] = useState("Password123!");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isLoading, error } = useLogin();
 
